7961441c4c23ef4039b1d468c67d07d4f228b33e
origotdaskill <mr.walid.ori@gmaill.com>
2024-02-17 00:39:28 +0100

    ori commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * selection_sort - Sorts an array of integers in ascending order
  *                   using the Selection sort algorithm
@@ -7,26 +8,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int temp;
+	size_t i, j, min_idx;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            temp = array[min_idx];
-            array[min_idx] = array[i];
-            array[i] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+		temp = array[min_idx];
+		array[min_idx] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		}
+	}
 }
