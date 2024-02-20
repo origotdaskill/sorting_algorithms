dbb03cc60b18fcbed325adcfbf8a4fa891dfd310
origotdaskill <mr.walid.ori@gmaill.com>
2024-02-20 19:18:11 +0100

    ori commit

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,73 +1,112 @@
 #include "deck.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * card_cmp - Compares two cards for sorting
- * @ptr1: Pointer to the first card
- * @ptr2: Pointer to the second card
- * Return: Integer less than, equal to, or greater than zero if the first
- * card is found, respectively, to be less than, to match, or be greater than
- * the second card.
- */
-int card_cmp(const void *ptr1, const void *ptr2)
-{
-    const card_t *card1 = (*((const deck_node_t **)ptr1))->card;
-    const card_t *card2 = (*((const deck_node_t **)ptr2))->card;
-
-    int value_cmp = strcmp(card1->value, card2->value);
-    if (value_cmp != 0)
-        return value_cmp;
-
-    return card1->kind - card2->kind;
-}
-
-/**
- * sort_deck - Sorts a deck of cards
- * @deck: Pointer to the head of the doubly linked list representing the deck
+ * sort_deck - sorts a deck of card
+ * @deck: doubly linked list to sort
  */
 void sort_deck(deck_node_t **deck)
 {
-    deck_node_t *current;
-    deck_node_t *temp;
-    size_t i, j;
-    size_t count = 0;
+	deck_node_t *curr;
+	size_t len;
+	deck_node_t *one, *two, *three, *four;
 
-    if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
-        return;
+	len = list_len_deck(*deck);
 
-    current = *deck;
-    while (current)
-    {
-        current = current->next;
-        count++;
-    }
+	if (!deck || !*deck || len < 2)
+		return;
 
-    for (i = 0; i < count - 1; i++)
-    {
-        current = *deck;
-        for (j = 0; j < count - i - 1; j++)
-        {
-            if (current->card->kind > current->next->card->kind ||
-                (current->card->kind == current->next->card->kind &&
-                 strcmp(current->card->value, current->next->card->value) > 0))
-            {
-                temp = current->next->next;
-                current->next->next = current;
-                current->next->prev = current->prev;
-                current->prev = current->next;
-                current->next = temp;
-                if (temp)
-                    temp->prev = current;
-                if (current->prev)
-                    current->prev->next = current->prev->next == current ? current->next : current;
-                if (current == *deck)
-                    *deck = current->prev;
-            }
-            else
-            {
-                current = current->next;
-            }
-        }
-    }
+	curr = *deck;
+	while (curr)
+	{
+		if (curr->prev && card_value(curr) < card_value(curr->prev))
+		{
+			one = curr->prev->prev;
+			two = curr->prev;
+			three = curr;
+			four = curr->next;
+
+			two->next = four;
+			if (four)
+				four->prev = two;
+			three->next = two;
+			three->prev = one;
+			if (one)
+				one->next = three;
+			else
+				*deck = three;
+			two->prev = three;
+			curr = *deck;
+			continue;
+		}
+		else
+			curr = curr->next;
+	}
+}
+
+/**
+ * card_value - returns the value of a card
+ * @node: card in a deck
+ *
+ * Return: value between 1 and 52
+ */
+int card_value(deck_node_t *node)
+{
+	char *val[13] = {"Ace", "2", "3", "4", "5", "6",
+		"7", "8", "9", "10", "Jack", "Queen", "King"};
+	char *kinds[4] = {"SPADE", "HEART", "CLUB", "DIAMOND"};
+	int i, kind_val = 0;
+
+	for (i = 1; i <= 13; i++)
+	{
+		if (!_strcmp(node->card->value, val[i - 1]))
+			kind_val = i;
+	}
+
+	for (i = 1; i <= 4; i++)
+	{
+		if (!_strcmp(kinds[node->card->kind], kinds[i - 1]))
+			kind_val = kind_val + (13 * i);
+	}
+
+	return (kind_val);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+/**
+ * list_len_deck - function returns length of list
+ * @list: head of list
+ *
+ * Return: length
+ */
+size_t list_len_deck(deck_node_t *list)
+{
+	size_t len = 0;
+
+	while (list)
+	{
+		len++;
+		list = list->next;
+	}
+	return (len);
 }
